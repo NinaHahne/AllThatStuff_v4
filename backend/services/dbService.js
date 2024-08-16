4672deefbb0949c61c6174cbc1b8448322632655
NinaHahne <ninamariehahne@gmail.com>
2024-08-16 19:00:28 +0200

    Temporarily disabled Lowdb

--- backend/services/dbService.js
+++ backend/services/dbService.js
@@ -1,19 +1,24 @@
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
+// import { Low } from 'lowdb';
+// import { JSONFile } from 'lowdb/node';
 
-// Define the path to the JSON file
-const adapter = new JSONFile('db.json');
+// // Define the path to the JSON file
+// const adapter = new JSONFile('db.json');
 
-// Create a new Lowdb instance
-const db = new Low(adapter);
+// // Create a new Lowdb instance
+// const db = new Low(adapter);
 
-// Read the data from the JSON file
-await db.read();
+// // Read the data from the JSON file
+// await db.read();
 
-// Ensure the database has a default structure if it is empty
-db.data ||= { games: [] }; //TODO: does not work
+// // Ensure the database has a default structure if it is empty
+// db.data ||= { games: [] }; //TODO: does not work
 
-// Write the default structure to the file if it was empty
-await db.write();
+// // Write the default structure to the file if it was empty
+// await db.write();
 
-export const getDb = () => db;
+// export const getDb = () => db;
+
+// ################################################
+
+// Temporarily replace the `getDb` function with a mock function that returns an empty object
+export const getDb = () => ({ games: [] });
